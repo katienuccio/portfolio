10c291dbc6656589f5625a2fc50850e0b84bd24f
Katie Nuccio <14321522+katienuccio@users.noreply.github.com>
2023-02-08 10:14:44 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,6 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBPIcViRaVid0q9lI2QPuRcBRRgBgYAolE",
-  authDomain: "katie-nuccio-portfolio.firebaseapp.com",
-  projectId: "katie-nuccio-portfolio",
-  storageBucket: "katie-nuccio-portfolio.appspot.com",
-  messagingSenderId: "750399836057",
-  appId: "1:750399836057:web:58c5be8bc67d537f920d08",
-  measurementId: "G-WJ9EWLDX3S"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
