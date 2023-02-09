b8c1e449c94ec3537eb18a62ef530355a3dda8a6
katie.nuccio <katie.nuccio@healthcarousel.com>
2023-02-09 13:14:40 -0500

    adding get in touch page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    message: new FormControl('')
+  })
+
+  getInTouch() {
+    const element = document.getElementById('get-in-touch');
+    element?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  submitForm() {
+    this.form.markAllAsTouched();
+    console.log(this.form);
+  }
+}
