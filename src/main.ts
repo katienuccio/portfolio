b0b0d4c092e5326012dbc64f0195f4a19bf7ddde
katie.nuccio <katie.nuccio@healthcarousel.com>
2023-03-22 20:53:36 -0400

    testing

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,17 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: environment.firebaseConfig.apiKey,
+  authDomain: environment.firebaseConfig.authDomain,
+  projectId: environment.firebaseConfig.projectId,
+  storageBucket: environment.firebaseConfig.storageBucket,
+  messagingSenderId: environment.firebaseConfig.messagingSenderId,
+  appId: environment.firebaseConfig.appId,
+  measurementId: environment.firebaseConfig.measurementId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
